e028daf3396a60751969f39216ad423ccc06c9b1
ShyhiemGilbert <42538473+ShyhiemGilbert@users.noreply.github.com>
2021-08-04 02:47:40 +0100

    Customised the layout, templates across multiple apps, Url parameters, raising 404 errors and referencing Urls

--- games/urls.py
+++ games/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from . import views # . means in current directory
+from . import views  # . means in current directory
+from . import apps
+
+
+app_name = 'games'
+
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:game_id>', views.detail, name='detail')  # games/1
+    #              /game/1
 ]
--- games/views.py
+++ games/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Game
 
 
@@ -8,3 +8,8 @@ from .models import Game
 def index(request):
     games = Game.objects.all()  # SELECT * FROM games_game
     return render(request, 'games/index.html', {'games': games})
+
+
+def detail(request, game_id):
+    game = get_object_or_404(Game, pk=game_id)
+    return render(request, 'games/detail.html', {'game': game})
