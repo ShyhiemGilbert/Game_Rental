cef512827ac76b6e840a06c0ff10627eb55bfda8
ShyhiemGilbert <42538473+ShyhiemGilbert@users.noreply.github.com>
2021-08-05 03:45:27 +0100

    Stuck on mapping homepage

--- Games4Rent/urls.py
+++ Games4Rent/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from api.models import GameResource
+from . import views # . from route folder (homepage))
 
 game_resource = GameResource()
 
 urlpatterns = [
+    path('', views.home),
     path('admin/', admin.site.urls),
     path('games/', include('games.urls')),
     path('api/', include(game_resource.urls))
